9c21feac65d541ec3780512dea1d45196935397f
chrystiansantos <chrystian_santos_silva@hotmail.com>
2021-03-19 19:56:08 -0300

    Criando component para o modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Modal from 'react-modal';
 import { useCallback, useState } from 'react';
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 Modal.setAppElement('#root');
 
@@ -22,12 +23,10 @@ export function App() {
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transacao</h2>
-      </Modal>
+      />
     </>
   );
 }
