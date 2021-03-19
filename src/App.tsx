30e779e2e2a3fa9a1efb7f5a9a705d3db2d364d7
chrystiansantos <chrystian_santos_silva@hotmail.com>
2021-03-19 19:41:06 -0300

    Criando modal criacao de transacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
+import Modal from 'react-modal';
+import { useCallback, useState } from 'react';
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [
+    isNewTransactionModalOpen,
+    setIsNewTransactionModalOpen,
+  ] = useState<boolean>(false);
+
+  const handleOpenNewTransactionModal = useCallback(() => {
+    setIsNewTransactionModalOpen(true);
+  }, []);
+
+  const handleCloseNewTransactionModal = useCallback(() => {
+    setIsNewTransactionModalOpen(false);
+  }, []);
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transacao</h2>
+      </Modal>
     </>
   );
 }
